6a222f91de2a93ebffa17e45e5540a3b518c49f6
SeheeMoon <anstpgml5035@ajou.ac.kr>
2022-12-14 01:45:35 +0900

    261 ~ 280

--- 271_280.py
+++ 271_280.py
@@ -199,46 +199,46 @@ class Account:
 Moon = Account("문세희", 1500000000)
 Moon.display_info()
 
-# 277
+# # 277
 
-class Account:
+# class Account:
 
-    account_count = 0
+#     account_count = 0
 
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.bank = "SC은행"
+#     def __init__(self, name, balance):
+#         self.name = name
+#         self.balance = balance
+#         self.bank = "SC은행"
 
-        num1 = randrange(0, 1000)
-        num2 = randrange(0, 100)
-        num3 = randrange(0, 1000000)
+#         num1 = randrange(0, 1000)
+#         num2 = randrange(0, 100)
+#         num3 = randrange(0, 1000000)
 
-        num1 = str(num1).zfill(3)
-        num2 = str(num2).zfill(2)
-        num3 = str(num3).zfill(6)
-        self.account = num1 + "-" + num2 + "-" + num3
+#         num1 = str(num1).zfill(3)
+#         num2 = str(num2).zfill(2)
+#         num3 = str(num3).zfill(6)
+#         self.account = num1 + "-" + num2 + "-" + num3
 
-        Account.account_count += 1
+#         Account.account_count += 1
 
-    def get_account_num(cls):
-        print(cls.account_count)
+#     def get_account_num(cls):
+#         print(cls.account_count)
 
-    def deposit(self, amount):
-        if amount >= 1:
-            self.balance += amount
-            print("{} current balance : {}".format(self.name, self.balance))
+#     def deposit(self, amount):
+#         if amount >= 1:
+#             self.balance += amount
+#             print("{} current balance : {}".format(self.name, self.balance))
     
-    def withdraw(self, amount2):
-        if self.balance >= amount2:
-            self.balance -= amount2
-            print("{} current balance : {}".format(self.name, self.balance))
+#     def withdraw(self, amount2):
+#         if self.balance >= amount2:
+#             self.balance -= amount2
+#             print("{} current balance : {}".format(self.name, self.balance))
 
-    def display_info(self):
-        print("은행이름: {}".format(self.bank))
-        print("예금주: {}".format(self.name))
-        print("계좌번호: {}".format(self.account))
-        print("잔액: {:,}".format(self.balance))
+#     def display_info(self):
+#         print("은행이름: {}".format(self.bank))
+#         print("예금주: {}".format(self.name))
+#         print("계좌번호: {}".format(self.account))
+#         print("잔액: {:,}".format(self.balance))
 
-    def interest(self):
-        
+
+
